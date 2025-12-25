69308fe2fd3f54a7cd971aa3f8bbc65bb8ee5bed
Harshthakur24 <thakur2004harsh@gmail.com>
2025-12-26 00:25:47 +0530

    Wrote the CI Pipeline yaml

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello DevOps")
+console.log("I am learing CI/CD using Github Actions")
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Start the Test")
+setTimeout(() => {
+    console.log("Waiting for 3 seconds")
+}, 3000)
+console.log("Test Completed")
